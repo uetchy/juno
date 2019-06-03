fb5b6350dc7c4aee2a78e66dcab9d71935461a8b
Yasuaki Uechi <y@uechi.io>
2019-06-03 16:57:34 +0900

    notarize app

--- scripts/post-build-mac.js
+++ scripts/post-build-mac.js
@@ -1,9 +1,28 @@
-const { join } = require('path')
+const path = require('path')
 const fs = require('fs')
 const plist = require('plist')
+const { notarize } = require('electron-notarize')
 
-exports.default = () => {
-  const plistPath = join(
+const configPath = path.resolve(__dirname, '../package.json')
+const config = require(configPath)
+
+const appleId = process.env.APPLE_ID
+const appleIdPassword = process.env.APPLE_PASSWORD
+const appPath = path.resolve(__dirname, '../dist/mac/Juno.app')
+const appBundleId = config.build.appId
+
+async function notarizeApp() {
+  console.log(appBundleId, appPath, appleId, appleIdPassword)
+  await notarize({
+    appBundleId,
+    appPath,
+    appleId,
+    appleIdPassword,
+  })
+}
+
+async function documentTypes() {
+  const plistPath = path.resolve(
     __dirname,
     '../dist/mac/Juno.app/Contents',
     'Info.plist'
@@ -22,4 +41,9 @@ exports.default = () => {
   fs.writeFileSync(plistPath, plist.build(appPlist))
 
   console.log('Modified bundle types in plist file')
+}
+
+exports.default = async () => {
+  await documentTypes()
+  await notarizeApp()
 }
